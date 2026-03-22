c5700802ae0b6e07241df85a3b5461da3a67d32f
sandhya kumari <sandhyasingh950823256@gmail.com>
2026-03-22 18:20:05 +0530

    feat: add hooks and state

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,33 +1,46 @@
-import React from 'react';
-import { useNavigate,Link } from 'react-router';
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router';
 import "../auth.form.scss"
+import { useAuth } from '../hooks/useAuth';
+
 
 const Login = () => {
 
-const handleSubmit = (e) => {
-	e.preventDefault()
-}
+	const { loading, handleLogin } = useAuth()
+
+	const [email, setEmail] = useState("")
+	const [password, setpassword] = useState("")
+
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		await handleLogin({ email, password })
+	}
+
+	if (loading) {
+		return (<main><h1>Loading.....</h1></main>)
+	}
 
 
 	return (
 		<main>
 			<div className="form-container">
 				<h1>Login</h1>
-
-
 				<form onSubmit={handleSubmit}>
-
 					<div className="input-group">
 						<label htmlFor="email">Email</label>
-						<input type="email" id='email' name='email' placeholder='Enter email address' />
+						<input
+							onChange={(e) => { setEmail(e.target.value) }}
+							type="email" id='email' name='email' placeholder='Enter email address' />
 					</div>
 
 					<div className="input-group">
 						<label htmlFor="password">Password</label>
-						<input type="password" id='password' name='password' placeholder='Enter password' />
+						<input
+							onChange={(e) => { setpassword(e.target.value) }}
+							type="password" id='password' name='password' placeholder='Enter password' />
 					</div>
 
-					<button className='button primary-button'>Login</button>
+					<button type="submit" className='button primary-button'>Login</button>
 
 				</form>
 
